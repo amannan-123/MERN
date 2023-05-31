3b2a8b40d5053f9f14f37c9b35915d22d82665bd
Abdul Mannan <tanveer6110@gmail.com>
2023-05-31 21:10:42 +0500

    Moved API URL to env file.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	//add proxy for backend
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-				changeOrigin: true,
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	return defineConfig({
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/api": {
+					target: process.env.VITE_API_URL,
+					changeOrigin: true,
+				},
 			},
+			host: true,
+			strictPort: true,
+			port: 5173,
 		},
-		host: true,
-		strictPort: true,
-		port: 5173,
-	},
-});
+	});
+};
